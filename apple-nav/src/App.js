60cbf83b32c7ac3b0c1a64dfa02556c52c98d301
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-17 18:52:34 -0500

    got my page to display, by installing the react router dom and used browserrouter on the app

---
+++ apple-nav/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+//create component function for each nav link 
+
+function App() {
+  return (
+    <div className="App">
+    {/* create the routes to the components */}
+    </div>
+  );
+}
+
+export default App;
